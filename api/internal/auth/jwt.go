072bb6c8102c65835ad6e9ea49f897bfd04c43ae
Alfin Akhret <alfin.akhret@gmail.com>
2026-03-11 15:45:43 +0700

    add function ParseToken

--- api/internal/auth/jwt.go
+++ api/internal/auth/jwt.go
@@ -1,12 +1,14 @@
 package auth
 
 import (
+	"errors"
 	"time"
 
 	"github.com/golang-jwt/jwt/v5"
 )
 
 var secret = []byte("supersecret")
+var ErrInvalidToken = errors.New("invalid token")
 
 type Claims struct {
 	UserID string `json:"user_id"`
@@ -24,4 +26,28 @@ func GenerateToken(userID string) (string, error) {
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
 
 	return token.SignedString(secret)
+}
+
+func ParseToken(tokenString string) (*Claims, error) {
+	token, err := jwt.ParseWithClaims(
+		tokenString,
+		&Claims{},
+		func(token *jwt.Token) (interface{}, error) {
+			if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+				return nil, ErrInvalidToken
+			}
+			return secret, nil
+		},
+	)
+
+	if err != nil {
+		return nil, err
+	}
+
+	claims, ok := token.Claims.(*Claims)
+	if !ok || !token.Valid {
+		return nil, ErrInvalidToken
+	}
+
+	return claims, nil
 }
