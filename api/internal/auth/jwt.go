171f64a9856d1a15d0caaf98035d35668e03de41
Alfin Akhret <alfin.akhret@gmail.com>
2026-03-11 14:50:51 +0700

    add function to create jwt token

---
+++ api/internal/auth/jwt.go
@@ -0,0 +1,27 @@
+package auth
+
+import (
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+)
+
+var secret = []byte("supersecret")
+
+type Claims struct {
+	UserID string `json:"user_id"`
+	jwt.RegisteredClaims
+}
+
+func GenerateToken(userID string) (string, error) {
+	claims := Claims{
+		UserID: userID,
+		RegisteredClaims: jwt.RegisteredClaims{
+			ExpiresAt: jwt.NewNumericDate(time.Now().Add(24 * time.Hour)),
+		},
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+
+	return token.SignedString(secret)
+}
