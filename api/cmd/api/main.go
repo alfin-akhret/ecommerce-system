f355808aff8992dd25c1bd90692a0f88a746c7f2
Alfin Akhret <alfin.akhret@gmail.com>
2026-04-21 20:24:32 +0700

    add prometheus scrapper and grafana dashboard

--- api/cmd/api/main.go
+++ api/cmd/api/main.go
@@ -25,69 +25,73 @@ func main() {
 	// create new logger
 	logger := helper.NewLogger()
 
-	// register middlewares
-	r.Use(helper.RecoveryMiddleware(logger))
-	r.Use(helper.RequestIDMiddleware)
-	r.Use(helper.LoggerMiddleware(logger))
-	r.Use(helper.AccessLogMiddleware(logger))
-	r.Use(helper.MetricsMiddleware)
-
-	// health check endpoint
-	r.Get("/health", func(w http.ResponseWriter, r *http.Request) {
-		if _, err := w.Write([]byte("OK cool")); err != nil {
-			log.Printf("health write failed: %v", err)
-		}
-	})
-
 	// metrics endpoint for prometheus
 	r.Handle("/metrics", promhttp.Handler())
 
-	r.Get("/users/{id}", helper.Handle(application.UserHandler.GetUser))
-	r.Post("/register", helper.Handle(application.UserHandler.Register))
-	r.Post("/login", helper.Handle(application.AuthHandler.Login))
+	r.Group(func(r chi.Router) {
 
-	// protected: user
-	r.With(auth.AuthMiddleware).Get("/me", helper.Handle(application.AuthHandler.Me))
+		// register middlewares
+		r.Use(helper.RecoveryMiddleware(logger))
+		r.Use(helper.RequestIDMiddleware)
+		r.Use(helper.LoggerMiddleware(logger))
+		r.Use(helper.AccessLogMiddleware(logger))
+		r.Use(helper.MetricsMiddleware)
 
-	// protected: order
-	r.With(auth.AuthMiddleware).Get("/orders", helper.Handle(application.OrderHandler.ListOrders))
-	r.With(auth.AuthMiddleware).Get("/orders/{id}", helper.Handle(application.OrderHandler.GetOrder))
-	r.With(auth.AuthMiddleware).Post("/create", helper.Handle(application.OrderHandler.CreateOrder))
-	r.With(auth.AuthMiddleware).Get("/checkout", helper.Handle(application.OrderHandler.Checkout))
+		// health check endpoint
+		r.Get("/health", func(w http.ResponseWriter, r *http.Request) {
+			if _, err := w.Write([]byte("OK cool")); err != nil {
+				log.Printf("health write failed: %v", err)
+			}
+		})
 
-	// protected: cart
-	r.With(auth.AuthMiddleware).Post("/cart", helper.Handle(application.CartHandler.AddItem))
-	r.With(auth.AuthMiddleware).Delete("/cart", helper.Handle(application.CartHandler.DeleteCart))
-	r.With(auth.AuthMiddleware).Delete("/cart/{product_id}", helper.Handle(application.CartHandler.RemoveItem))
-	r.With(auth.AuthMiddleware).Patch("/cart", helper.Handle(application.CartHandler.UpdateQuantity))
-	r.With(auth.AuthMiddleware).Get("/cart", helper.Handle(application.CartHandler.GetCart))
+		r.Get("/users/{id}", helper.Handle(application.UserHandler.GetUser))
+		r.Post("/register", helper.Handle(application.UserHandler.Register))
+		r.Post("/login", helper.Handle(application.AuthHandler.Login))
 
-	// Payments route
-	r.Route("/payments", func(r chi.Router) {
-		r.With(auth.AuthMiddleware).Post("/", helper.Handle(application.PaymentHandler.CreatePayment))
-		r.With(auth.AuthMiddleware).Get("/{payment_id}", helper.Handle(application.PaymentHandler.GetPayment))
-		r.With(auth.AuthMiddleware).Patch("/{payment_id}", helper.Handle(application.PaymentHandler.UpdatePaymentStatus))
-		r.Post("/callback", helper.Handle(application.PaymentHandler.HandleCallback))
-		r.Post("/{payment_id}/success", helper.Handle(application.PaymentHandler.ProcessPaymentSuccess))
-		r.Post("/{payment_id}/fail", helper.Handle(application.PaymentHandler.ProcessPaymentFailed))
+		// protected: user
+		r.With(auth.AuthMiddleware).Get("/me", helper.Handle(application.AuthHandler.Me))
+
+		// protected: order
+		r.With(auth.AuthMiddleware).Get("/orders", helper.Handle(application.OrderHandler.ListOrders))
+		r.With(auth.AuthMiddleware).Get("/orders/{id}", helper.Handle(application.OrderHandler.GetOrder))
+		r.With(auth.AuthMiddleware).Post("/create", helper.Handle(application.OrderHandler.CreateOrder))
+		r.With(auth.AuthMiddleware).Get("/checkout", helper.Handle(application.OrderHandler.Checkout))
+
+		// protected: cart
+		r.With(auth.AuthMiddleware).Post("/cart", helper.Handle(application.CartHandler.AddItem))
+		r.With(auth.AuthMiddleware).Delete("/cart", helper.Handle(application.CartHandler.DeleteCart))
+		r.With(auth.AuthMiddleware).Delete("/cart/{product_id}", helper.Handle(application.CartHandler.RemoveItem))
+		r.With(auth.AuthMiddleware).Patch("/cart", helper.Handle(application.CartHandler.UpdateQuantity))
+		r.With(auth.AuthMiddleware).Get("/cart", helper.Handle(application.CartHandler.GetCart))
+
+		// Payments route
+		r.Route("/payments", func(r chi.Router) {
+			r.With(auth.AuthMiddleware).Post("/", helper.Handle(application.PaymentHandler.CreatePayment))
+			r.With(auth.AuthMiddleware).Get("/{payment_id}", helper.Handle(application.PaymentHandler.GetPayment))
+			r.With(auth.AuthMiddleware).Patch("/{payment_id}", helper.Handle(application.PaymentHandler.UpdatePaymentStatus))
+			r.Post("/callback", helper.Handle(application.PaymentHandler.HandleCallback))
+			r.Post("/{payment_id}/success", helper.Handle(application.PaymentHandler.ProcessPaymentSuccess))
+			r.Post("/{payment_id}/fail", helper.Handle(application.PaymentHandler.ProcessPaymentFailed))
+		})
+
+		r.Route("/products", func(r chi.Router) {
+			r.Get("/", helper.Handle(application.ProductHandler.ListProducts))
+			r.Get("/{id}", helper.Handle(application.ProductHandler.GetProduct))
+
+			// protected: khusus admin/internal
+			r.With(auth.AuthMiddleware, auth.AdminMiddleware).Post("/users", helper.Handle(application.UserHandler.CreateUser))
+			r.With(auth.AuthMiddleware, auth.AdminMiddleware).Post("/", helper.Handle(application.ProductHandler.CreateProduct))
+			r.With(auth.AuthMiddleware, auth.AdminMiddleware).Patch("/{id}/stock", helper.Handle(application.ProductHandler.UpdateStock))
+			r.With(auth.AuthMiddleware, auth.AdminMiddleware).Post("/{id}/reserve", helper.Handle(application.ProductHandler.ReserveStock))
+			r.With(auth.AuthMiddleware, auth.AdminMiddleware).Post("/{id}/release", helper.Handle(application.ProductHandler.ReleaseStock))
+			r.With(auth.AuthMiddleware, auth.AdminMiddleware).Post("/{id}/confirm", helper.Handle(application.ProductHandler.ConfirmStock))
+
+		})
+
+		// panic test
+		r.Get("/panic", PanicHandler)
+
 	})
-
-	r.Route("/products", func(r chi.Router) {
-		r.Get("/", helper.Handle(application.ProductHandler.ListProducts))
-		r.Get("/{id}", helper.Handle(application.ProductHandler.GetProduct))
-
-		// protected: khusus admin/internal
-		r.With(auth.AuthMiddleware, auth.AdminMiddleware).Post("/users", helper.Handle(application.UserHandler.CreateUser))
-		r.With(auth.AuthMiddleware, auth.AdminMiddleware).Post("/", helper.Handle(application.ProductHandler.CreateProduct))
-		r.With(auth.AuthMiddleware, auth.AdminMiddleware).Patch("/{id}/stock", helper.Handle(application.ProductHandler.UpdateStock))
-		r.With(auth.AuthMiddleware, auth.AdminMiddleware).Post("/{id}/reserve", helper.Handle(application.ProductHandler.ReserveStock))
-		r.With(auth.AuthMiddleware, auth.AdminMiddleware).Post("/{id}/release", helper.Handle(application.ProductHandler.ReleaseStock))
-		r.With(auth.AuthMiddleware, auth.AdminMiddleware).Post("/{id}/confirm", helper.Handle(application.ProductHandler.ConfirmStock))
-
-	})
-
-	// panic test
-	r.Get("/panic", PanicHandler)
 
 	// run worker
 	ctx := context.Background()
--- api/pkg/helper/metrics.go
+++ api/pkg/helper/metrics.go
@@ -9,40 +9,42 @@ import (
 	"github.com/prometheus/client_golang/prometheus"
 )
 
-// 1. Traffic
-var httpRequestsTotal = prometheus.NewCounterVec(
-	prometheus.CounterOpts{
-		Name: "http_requests_total",
-		Help: "total number of http requests",
-	},
-	[]string{"method", "path", "status"},
-)
+var (
+	// 1. Traffic
+	httpRequestsTotal = prometheus.NewCounterVec(
+		prometheus.CounterOpts{
+			Name: "http_requests_total",
+			Help: "total number of http requests",
+		},
+		[]string{"method", "path", "status"},
+	)
 
-// 2. latency
-var httpRequestDuration = prometheus.NewHistogramVec(
-	prometheus.HistogramOpts{
-		Name:    "http_request_duration_seconds",
-		Help:    "HTTP request latency",
-		Buckets: prometheus.DefBuckets,
-	},
-	[]string{"method", "path", "status"},
-)
+	// 2. latency
+	httpRequestDuration = prometheus.NewHistogramVec(
+		prometheus.HistogramOpts{
+			Name:    "http_request_duration_seconds",
+			Help:    "HTTP request latency",
+			Buckets: prometheus.DefBuckets,
+		},
+		[]string{"method", "path", "status"},
+	)
 
-// 3. errors
-var httpErrorsTotal = prometheus.NewCounterVec(
-	prometheus.CounterOpts{
-		Name: "http_errors_total",
-		Help: "Total HTTP errors",
-	},
-	[]string{"method", "path", "status"},
-)
+	// 3. errors
+	httpErrorsTotal = prometheus.NewCounterVec(
+		prometheus.CounterOpts{
+			Name: "http_errors_total",
+			Help: "Total HTTP errors",
+		},
+		[]string{"method", "path", "status"},
+	)
 
-// 4. server load
-var httpInFlight = prometheus.NewGauge(
-	prometheus.GaugeOpts{
-		Name: "http_requests_in_flight",
-		Help: "Current in-flight requests",
-	},
+	// 4. server load
+	httpInFlight = prometheus.NewGauge(
+		prometheus.GaugeOpts{
+			Name: "http_requests_in_flight",
+			Help: "Current in-flight requests",
+		},
+	)
 )
 
 func init() {
@@ -58,15 +60,10 @@ func init() {
 func MetricsMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		// exclude path named "metrics"
-		if r.URL.Path == "/metrics" {
-			next.ServeHTTP(w, r)
-			return
-		}
-
-		routePattern := chi.RouteContext(r.Context()).RoutePattern()
-		if routePattern == "" {
-			routePattern = "unknown"
-		}
+		//if r.URL.Path == "/metrics" {
+		//	next.ServeHTTP(w, r)
+		//	return
+		//}
 
 		start := time.Now()
 
@@ -75,30 +72,33 @@ func MetricsMiddleware(next http.Handler) http.Handler {
 
 		rec := &responseRecorder{
 			ResponseWriter: w,
-			statusCode:     200,
+			statusCode:     http.StatusOK,
 		}
 
 		next.ServeHTTP(rec, r)
 
+		status := strconv.Itoa(rec.statusCode)
 		duration := time.Since(start).Seconds()
+
+		route := getRoute(r)
 
 		httpRequestsTotal.WithLabelValues(
 			r.Method,
-			routePattern,
-			strconv.Itoa(rec.statusCode),
+			route,
+			status,
 		).Inc()
 
 		httpRequestDuration.WithLabelValues(
 			r.Method,
-			routePattern,
-			strconv.Itoa(rec.statusCode),
+			route,
+			status,
 		).Observe(duration)
 
 		if rec.statusCode >= 400 {
 			httpErrorsTotal.WithLabelValues(
 				r.Method,
-				routePattern,
-				strconv.Itoa(rec.statusCode),
+				route,
+				status,
 			).Inc()
 		}
 	})
@@ -112,4 +112,21 @@ type responseRecorder struct {
 func (r *responseRecorder) WriteHeader(statusCode int) {
 	r.statusCode = statusCode
 	r.ResponseWriter.WriteHeader(statusCode)
+}
+
+func getRoute(r *http.Request) string {
+	route := chi.RouteContext(r.Context()).RoutePattern()
+
+	if route != "" {
+		return route
+	}
+
+	switch r.URL.Path {
+	case "/metrics":
+		return "/metrics"
+	case "/health":
+		return "/health"
+	default:
+		return "unmatched"
+	}
 }
