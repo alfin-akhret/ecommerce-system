0102ae096e1dcd06325024b7899c5a9caffa1682
Alfin Akhret <alfin.akhret@gmail.com>
2026-04-21 19:02:25 +0700

    add metrics: errors, latency, server load

--- api/pkg/helper/metrics.go
+++ api/pkg/helper/metrics.go
@@ -3,31 +3,75 @@ package helper
 import (
 	"net/http"
 	"strconv"
+	"time"
 
+	"github.com/go-chi/chi"
 	"github.com/prometheus/client_golang/prometheus"
 )
 
+// 1. Traffic
 var httpRequestsTotal = prometheus.NewCounterVec(
 	prometheus.CounterOpts{
 		Name: "http_requests_total",
 		Help: "total number of http requests",
 	},
-	[]string{"method", "endpoint", "status"},
+	[]string{"method", "path", "status"},
+)
+
+// 2. latency
+var httpRequestDuration = prometheus.NewHistogramVec(
+	prometheus.HistogramOpts{
+		Name:    "http_request_duration_seconds",
+		Help:    "HTTP request latency",
+		Buckets: prometheus.DefBuckets,
+	},
+	[]string{"method", "path", "status"},
+)
+
+// 3. errors
+var httpErrorsTotal = prometheus.NewCounterVec(
+	prometheus.CounterOpts{
+		Name: "http_errors_total",
+		Help: "Total HTTP errors",
+	},
+	[]string{"method", "path", "status"},
+)
+
+// 4. server load
+var httpInFlight = prometheus.NewGauge(
+	prometheus.GaugeOpts{
+		Name: "http_requests_in_flight",
+		Help: "Current in-flight requests",
+	},
 )
 
 func init() {
-	prometheus.MustRegister(httpRequestsTotal)
+	prometheus.MustRegister(
+		httpRequestsTotal,
+		httpRequestDuration,
+		httpErrorsTotal,
+		httpInFlight,
+	)
 }
 
 // metrics middleware
 func MetricsMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-
-		// exclude endpoints named "metrics"
+		// exclude path named "metrics"
 		if r.URL.Path == "/metrics" {
 			next.ServeHTTP(w, r)
 			return
 		}
+
+		routePattern := chi.RouteContext(r.Context()).RoutePattern()
+		if routePattern == "" {
+			routePattern = "unknown"
+		}
+
+		start := time.Now()
+
+		httpInFlight.Inc()
+		defer httpInFlight.Dec()
 
 		rec := &responseRecorder{
 			ResponseWriter: w,
@@ -36,11 +80,27 @@ func MetricsMiddleware(next http.Handler) http.Handler {
 
 		next.ServeHTTP(rec, r)
 
+		duration := time.Since(start).Seconds()
+
 		httpRequestsTotal.WithLabelValues(
 			r.Method,
-			r.URL.Path,
+			routePattern,
 			strconv.Itoa(rec.statusCode),
 		).Inc()
+
+		httpRequestDuration.WithLabelValues(
+			r.Method,
+			routePattern,
+			strconv.Itoa(rec.statusCode),
+		).Observe(duration)
+
+		if rec.statusCode >= 400 {
+			httpErrorsTotal.WithLabelValues(
+				r.Method,
+				routePattern,
+				strconv.Itoa(rec.statusCode),
+			).Inc()
+		}
 	})
 }
 
