736cc006fc66223b4e6a560aea76fd9ece627a7f
Alfin Akhret <alfin.akhret@gmail.com>
2026-04-30 18:00:46 +0700

    add timeout 2 second when creating order

--- api/internal/order/service.go
+++ api/internal/order/service.go
@@ -247,6 +247,11 @@ func (s *Service) getCart(ctx context.Context, userID string) (*Cart, error) {
 }
 
 func (s *Service) CreateOrder(ctx context.Context, userID string, req CreateOrderRequest, key string) (*CreateOrderResponse, error) {
+	// set timeout
+	// ini untuk menjaga external call seperti ke: db, redis,
+	ctx, cancel := context.WithTimeout(ctx, 2*time.Second)
+	defer cancel()
+
 	log := helper.LoggerFromCtx(ctx)
 	lUserID := zap.String("user_id", userID)
 
