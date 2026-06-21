8289d3b34f79feb8b435b700ce4d721d70014144
Alfin Akhret <alfin.akhret@gmail.com>
2026-06-21 11:56:49 +0700

    fix some migration

--- api/migrations/000026_alter_table_outbox_add_more_column.down.sql
+++ api/migrations/000026_alter_table_outbox_add_more_column.down.sql
@@ -1,6 +1,6 @@
 ALTER TABLE outbox
 DROP COLUMN retry_count,
-DROP COLUMN next_attemt_at, 
+DROP COLUMN next_attempt_at, 
 DROP COLUMN locked_at, 
-DROP COLUMN locker_by,
+DROP COLUMN locked_by,
 DROP COLUMN last_error;
