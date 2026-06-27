3d9597ca846534733253adf194d1dcc437c6bb46
Alfin Akhret <alfin.akhret@gmail.com>
2026-06-27 20:53:57 +0700

    add GetPending and MarkProcessed to event inbox_repository

--- api/internal/events/inbox_repository.go
+++ api/internal/events/inbox_repository.go
@@ -2,6 +2,7 @@ package events
 
 import (
 	"context"
+	"errors"
 
 	"github.com/alfin-akhret/ecommerce-system/internal/platform/database"
 	"github.com/alfin-akhret/ecommerce-system/pkg/helper"
@@ -13,10 +14,71 @@ type InboxRepository struct {
 
 type InboxRepositoryManager interface {
 	Save(ctx context.Context, event InboxEvent) error
+	GetPending(ctx context.Context, limit int) ([]InboxEvent, error)
+	MarkProcessed(ctx context.Context, id string) error
 }
 
 func NewInboxRepository(db database.DBTX) *InboxRepository {
 	return &InboxRepository{db: db}
+}
+
+func (ir *InboxRepository) MarkProcessed(ctx context.Context, id string) error {
+	query := `
+	UPDATE inbox_events
+	SET 
+		status = 'PROCESSED',
+		processed_at = NOW()
+	WHERE id = $1;
+	`
+
+	cmd, err := ir.db.Exec(ctx, query, id)
+	if err != nil {
+		return err
+	}
+
+	if cmd.RowsAffected() == 0 {
+		return errors.New("Event not found.")
+	}
+
+	return nil
+}
+
+func (ir *InboxRepository) GetPending(ctx context.Context, limit int) ([]InboxEvent, error) {
+	query := `
+	SELECT
+		id,
+		event_type,
+		payload,
+		created_at,
+		retry_count
+	FROM inbox_events
+	WHERE status = 'PENDING'
+	ORDER BY received_at
+	LIMIT $1;
+	`
+
+	rows, err := ir.db.Query(ctx, query, limit)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var inbox_events []InboxEvent
+	for rows.Next() {
+		var inbox_event InboxEvent
+		if err := rows.Scan(
+			&inbox_event.ID,
+			&inbox_event.EventType,
+			&inbox_event.Payload,
+			&inbox_event.CreatedAt,
+			&inbox_event.RetryCount,
+		); err != nil {
+			return nil, err
+		}
+		inbox_events = append(inbox_events, inbox_event)
+	}
+
+	return inbox_events, nil
 }
 
 func (ir *InboxRepository) Save(ctx context.Context, event InboxEvent) error {
